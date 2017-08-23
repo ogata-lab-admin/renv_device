f783773f5ac94292bb19a7fb00f1c2613effb4e0
ysuga <ysuga@ysuga.net>
2017-08-23 22:19:56 +0900

    dev id test imple

--- test/test_renv_device.py
+++ test/test_renv_device.py
@@ -8,17 +8,24 @@ import renv_device
 class TestRenvDevice(unittest.TestCase):
     """test class of tashizan.py
     """
+    
+    def setUp(self):
+        self._devId = "uuid-1234-5678-9012"
+        self._devTypeId = "WEB.DEVICE.TEST_DEVICE"
+        self._devName = "name-renv-device-name-for-test"
+        self._rd = renv_device.RenvDevice(self._devId, self._devTypeId, self._devName)
 
     def test_givenNameIsEqualToDeviceInfoName(self):
         """
         """
-        devId = "dev"
-        uuid = "uuid"
-        name = "name_renv_device_id_for_test"
-        rd = renv_device.RenvDevice(devId, uuid, name)
+        deviceInfo = self._rd.getDeviceInfo()
+        self.assertEqual(self._devName, deviceInfo['deviceName'])
 
-        deviceInfo = rd.getDeviceInfo()
-        self.assertEqual(name, deviceInfo['deviceName'])
+    def test_givenIdIsEqualToDeviceInfoName(self):
+        """
+        """
+        deviceInfo = self._rd.getDeviceInfo()
+        self.assertEqual(self._devId, deviceInfo['deviceId'])
 
 if __name__ == "__main__":
     unittest.main()
