11be1dcdf6594bb6ab2da7d285a71b84769b9598
ysuga <ysuga@ysuga.net>
2017-08-23 22:26:24 +0900

    test passed

--- test/test_renv_device.py
+++ test/test_renv_device.py
@@ -5,7 +5,7 @@
 import unittest
 import renv_device
 
-class TestRenvDevice(unittest.TestCase):
+class TestRenvDeviceInfo(unittest.TestCase):
     """test class of tashizan.py
     """
     
@@ -26,6 +26,44 @@ class TestRenvDevice(unittest.TestCase):
         """
         deviceInfo = self._rd.getDeviceInfo()
         self.assertEqual(self._devId, deviceInfo['deviceId'])
+
+
+class TestRenvDeviceInfo(unittest.TestCase):
+    """test class of tashizan.py
+    """
+
+    class MyRenvDevice(renv_device.RenvDevice):
+        def __init__(self, typeId, id, name):
+            renv_device.RenvDevice.__init__(self, typeId, id, name):
+            pass
+
+
+        def onSetup(self):
+            print 'onSetup is called'
+            pass
+
+        
+            
+    def setUp(self):
+        self._devId = "uuid-1234-5678-9012"
+        self._devTypeId = "WEB.DEVICE.TEST_DEVICE"
+        self._devName = "name-renv-device-name-for-test"
+        self._rd = MyRenvDevice(self._devTypeId, self._devId, self._devName)
+
+    def test_givenNameIsEqualToDeviceInfoName(self):
+        """
+        """
+        deviceInfo = self._rd.getDeviceInfo()
+        self.assertEqual(self._devName, deviceInfo['deviceName'])
+
+    def test_givenIdIsEqualToDeviceInfoName(self):
+        """
+        """
+        deviceInfo = self._rd.getDeviceInfo()
+        self.assertEqual(self._devId, deviceInfo['deviceId'])
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
